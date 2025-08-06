31cfb6d1534af1fb5dcc454d9b89f65cdf3007be
Bruno Pinheiro <brunobp07@gmail.com>
2025-08-06 15:55:26 -0400

    feat(ds/binary_tree): implement display Levelorder, Max and Contains

--- ds/binary_tree.go
+++ ds/binary_tree.go
@@ -53,3 +53,46 @@ func (t *BinaryTree) DisplayPostorder() string {
 	result += fmt.Sprintf("%d", t.value)
 	return result
 }
+
+func (t *BinaryTree) DisplayLevelorder() string {
+	result := ""
+	stack := []*BinaryTree{t}
+	for len(stack) > 0 {
+		if len(result) > 0 {
+			result += " "
+		}
+		node := stack[0]
+		result += fmt.Sprintf("%d", node.value)
+		if node.left != nil {
+			stack = append(stack, node.left)
+		}
+		if node.right != nil {
+			stack = append(stack, node.right)
+		}
+		if len(stack) > 0 {
+			stack = stack[1:]
+		}
+	}
+	return result
+}
+
+func (t *BinaryTree) Max() int {
+	maxValue := t.value
+	if t.left != nil {
+		leftMax := t.left.Max()
+		if leftMax > maxValue {
+			maxValue = leftMax
+		}
+	}
+	if t.right != nil {
+		rightMax := t.right.Max()
+		if rightMax > maxValue {
+			maxValue = rightMax
+		}
+	}
+	return maxValue
+}
+
+func (t *BinaryTree) Contains(value int) bool {
+	return t.value == value || (t.left != nil && t.left.Contains(value)) || (t.right != nil && t.right.Contains(value))
+}
--- ds/binary_tree_test.go
+++ ds/binary_tree_test.go
@@ -61,4 +61,59 @@ func TestBinaryTree(t *testing.T) {
 
 		require.Equal(t, "4 5 2 6 7 3 1", bt.DisplayPostorder())
 	})
+
+	t.Run("display levelorder (bft)", func(t *testing.T) {
+		bt := ds.NewBinaryTree(
+			1,
+			ds.NewBinaryTree(
+				2,
+				ds.NewBinaryTree(4, nil, nil),
+				ds.NewBinaryTree(5, nil, nil),
+			),
+			ds.NewBinaryTree(
+				3,
+				ds.NewBinaryTree(6, nil, nil),
+				ds.NewBinaryTree(7, nil, nil),
+			),
+		)
+
+		require.Equal(t, "1 2 3 4 5 6 7", bt.DisplayLevelorder())
+	})
+
+	t.Run("max element", func(t *testing.T) {
+		bt := ds.NewBinaryTree(
+			6,
+			ds.NewBinaryTree(
+				7,
+				ds.NewBinaryTree(3, nil, nil),
+				ds.NewBinaryTree(4, nil, nil),
+			),
+			ds.NewBinaryTree(
+				3,
+				ds.NewBinaryTree(5, nil, nil),
+				ds.NewBinaryTree(2, nil, nil),
+			),
+		)
+
+		require.Equal(t, 7, bt.Max())
+	})
+
+	t.Run("contains", func(t *testing.T) {
+		bt := ds.NewBinaryTree(
+			6,
+			ds.NewBinaryTree(
+				7,
+				ds.NewBinaryTree(3, nil, nil),
+				ds.NewBinaryTree(4, nil, nil),
+			),
+			ds.NewBinaryTree(
+				3,
+				ds.NewBinaryTree(5, nil, nil),
+				ds.NewBinaryTree(2, nil, nil),
+			),
+		)
+
+		require.False(t, bt.Contains(9))
+		require.True(t, bt.Contains(3))
+	})
 }
